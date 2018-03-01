e143af2bee1018a6d3b5328285730e2cc3382cb0
tejtharang <tharangd95@gmail.com>
2018-03-01 15:57:04 -0700

    front end of the main form complete. all elements received by the main app

--- app.js
+++ app.js
@@ -2,41 +2,29 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/mcmsc");
+/*var schema = new mongoose.Schema({
+
+}) */
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
-// Database connection
-var mongoose = require("mongoose");
-options = {
-    "auth": {
-        "authSource": "admin"
-    },
-    "user": "admin",
-    "pass": "admin123"
-};
-mongoose.connect("mongodb://localhost/cat_app",options);
+
 
 //Defining database schema
-var nameSchema = new mongoose.Schema({
-	firstName: String,
-	lastName: String
-});
-
-var user = mongoose.model("user",nameSchema);
-
 app.post("/addData",function(req,res){
-	console.log("reached add data");
-	var myData = new user(req.body);
-	myData.save().then(item => {
-		res.send("item saved to database");
-	})
-	.catch(err => {
-		res.status(400).send("unable to save to database");
-	});
+	var myData = req.body;
+	console.log(req.body);
+	
+	res.send("item saved to database");
 });
 
 app.get("*",function(req,res){
 	res.render("form.ejs");
 });
 
-app.listen(3000,"localhost");
+app.listen(3000,"localhost",function(){
+	console.log("connected!");
+});
