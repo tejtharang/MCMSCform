2f818a0511146013097dd58e73e03688e262a6ce
Tej Dandala <tej@Tejs-MacBook-Pro.local>
2018-03-08 23:01:03 -0700

    Email capability enabled. Director can now view, approve/disapprove and comment on students' progress

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+// setting up node email
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'tharangd95@gmail.com',
+    pass: 'tanicky9596'
+  }
+});
 var mongoose = require("mongoose");
 var mongoClient = require("mongodb").MongoClient;
 mongoose.Promise = global.Promise;
@@ -31,9 +40,13 @@ var schema = new mongoose.Schema({
 	metChair: String,
   reasonNotMetChair: String,
   prospectusDefenseCompleted: String,
+  prospectusAnticipatedDateSession: String,
+  prospectusAnticipatedDateYear: String,
   mtbiParticipation: String,
   mtbiParticipationYear: String,
   mtbiParticipationRole: String,
+  mtbiAnticipatedDateSession: String,
+  mtbiAnticipatedDateYear:String,
   publicationsExist: String,
   publicationAddTableHiddenInput: String,
   publicationName: String,
@@ -59,6 +72,48 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
 
+//director submission
+app.post("/directorSubmission",function(req,res){
+  console.log(req.body);
+  mongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mcmsc");
+    var myquery = { idNumber : req.body.studentId };
+    var email = req.body.asurite + "@asu.edu";
+    console.log(email);
+    
+    var msg = "";
+    if(req.body.studentProgressApproval == '1')
+    {
+      msg = "Your MCMSC form has been approved by the director";
+    }
+    else{
+      msg = "Your MCMSC form has been rejected by the director.\n" + "Director's comments: " + req.body.directorComment  + ".\nPlease schedule an appointment with the director as soon as possible. Thank you"
+    }
+    var newvalues = { $set: {directorComment : req.body.directorComment, directorApproval: req.body.studentProgressApproval } };
+    dbo.collection("students").updateOne(myquery, newvalues, function(err, result) {
+      if (err) throw err;
+      console.log("1 document updated");
+      res.redirect("/adminView");
+      var mailOptions = {
+        from: 'tharangd95@gmail.com',
+        to: email,
+        subject: 'MCMSC form submission',
+        text: msg
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      db.close();
+    });
+   
+  });
+  
+});
 // individual student data from adminView
 app.get("/adminView/:idno",function(req,res){
   var id = req.params.idno;
@@ -69,7 +124,7 @@ app.get("/adminView/:idno",function(req,res){
     dbo.collection("students").find({idNumber : id}).toArray(function(err,result){
       if (err) throw err;
       app.locals.result = result;
-      //console.log(result[0]);
+      console.log(result[0]);
       //var st = JSON.parse(result[0].courseAddTableHiddenInput);
       //console.log(st);
       res.render("adminViewEachStudent.ejs");
@@ -100,12 +155,25 @@ app.get("/adminView",function(req,res){
 app.post("/addData",function(req,res){
 	var myData = req.body;
 	console.log(req.body);
-	var myData = new Student(req.body);
+  var myData = new Student(req.body);
+  var email = req.body.asurite + '@asu.edu';
     myData.save()
       .then(item => {
-        console.log("hello there");
+        console.log(req.body);
         res.send("item saved to database");
-        
+        var mailOptions = {
+          from: 'tharangd95@gmail.com',
+          to: email,
+          subject: 'MCMSC form submission',
+          text: 'Thank you for submitting the form!'
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
       })
       .catch(err => {
         res.status(400).send("unable to save to database");
